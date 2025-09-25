2d9425dc8f45ba6e12ee1ec1636f694f51c379e3
thuylinh412004 <thuylinh412004@gmail.com>
2025-09-25 16:26:46 +0700

    Init localStorage

--- form_user.php
+++ form_user.php
@@ -1,6 +1,4 @@
 <?php
-// Start the session
-session_start();
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
 
@@ -11,7 +9,6 @@ if (!empty($_GET['id'])) {
     $_id = $_GET['id'];
     $user = $userModel->findUserById($_id);//Update existing user
 }
-
 
 if (!empty($_POST['submit'])) {
 
@@ -26,36 +23,40 @@ if (!empty($_POST['submit'])) {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>User form</title>
     <?php include 'views/meta.php' ?>
 </head>
+
 <body>
     <?php include 'views/header.php'?>
     <div class="container">
 
-            <?php if ($user || !isset($_id)) { ?>
-                <div class="alert alert-warning" role="alert">
-                    User form
-                </div>
-                <form method="POST">
-                    <input type="hidden" name="id" value="<?php echo $_id ?>">
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input class="form-control" name="name" placeholder="Name" value='<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>'>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password">
-                    </div>
+        <?php if ($user || !isset($_id)) { ?>
+        <div class="alert alert-warning" role="alert">
+            User form
+        </div>
+        <form method="POST">
+            <input type="hidden" name="id" value="<?php echo $_id ?>">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input class="form-control" name="name" placeholder="Name"
+                    value='<?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?>'>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Password">
+            </div>
 
-                    <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-                </form>
-            <?php } else { ?>
-                <div class="alert alert-success" role="alert">
-                    User not found!
-                </div>
-            <?php } ?>
+            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <?php } else { ?>
+        <div class="alert alert-success" role="alert">
+            User not found!
+        </div>
+        <?php } ?>
     </div>
 </body>
+
 </html>
--- logout.php
+++ logout.php
@@ -1,5 +1,19 @@
-<?php
-session_start();
-session_destroy();
-header('location: login.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>logout</title>
+</head>
+
+<body>
+    <script>
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('loggedUserId');
+    localStorage.removeItem('loggedUserName');
+    window.location.href = 'login.php';
+    </script>
+</body>
+
+</html>
